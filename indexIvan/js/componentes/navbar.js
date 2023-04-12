6b05de86bfd177dcb8a2d33aaabe27a61be94f88
unknown <ivan.alm99@gmail.com>
2023-04-11 23:02:33 -0500

    cambios totales

--- indexIvan/js/componentes/navbar.js
+++ indexIvan/js/componentes/navbar.js
@@ -1,6 +1,13 @@
 class Navbar extends HTMLElement{
     constructor(){
         super();
+        this.attachShadow({mode:'open'});
+    }
+    getTemplate(){
+        const template=document.createElement('template');
+        template.innerHTML=`
         
+        
+        `
     }
 }
